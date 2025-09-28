de874cd8ed17649b776a552b5aea47463f4b6f2c
Zock <zock868@gmail.com>
2025-09-28 11:14:29 +0200

    Working NPCs With UI

--- Assets/Scribts/ViligerScrbt.cs
+++ Assets/Scribts/ViligerScrbt.cs
@@ -1,54 +1,125 @@
+using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class ViligerScrbt : MonoBehaviour
 {
-    [SerializeField] private GameObject ViligerMenPrefab;
-    [SerializeField] private GameObject ViligerFemPrefab;
     [SerializeField] private GameObject ViligerParent;
-    [SerializeField] private int MaxViligers;
+    [SerializeField] private Collider2D Player;
     [SerializeField] private List<Transform> WalkToPoints;
-    void Start()
+    [SerializeField] private GameObject SellUI;
+    [SerializeField] private Button CloseSellUI;
+    private InputSystem_Actions InputSystem;
+
+    private Dictionary<NavMeshAgent, bool> isWaiting = new Dictionary<NavMeshAgent, bool>();
+    void Awake()
     {
-        for (int i = 0; i < MaxViligers; i++)
-        {
-            int X = Random.Range(0, 2);
-            switch (X)
-            {
-                case 0:
-                    GameObject M = Instantiate(ViligerMenPrefab);
-                    M.transform.position = WalkToPoints[Random.Range(0, WalkToPoints.Count)].position;
-                    M.transform.SetParent(ViligerParent.transform);
-                    break;
-                case 1:
-                    GameObject F = Instantiate(ViligerFemPrefab);
-                    F.transform.position = WalkToPoints[Random.Range(0, WalkToPoints.Count)].position;
-                    F.transform.SetParent(ViligerParent.transform);
-                    break;
-            }
-
-        }
-
+        InputSystem = new InputSystem_Actions();
+    }
+    private void OnEnable()
+    {
+        InputSystem.Enable(); // Input System aktivieren
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        InputSystem.Disable(); // Input System deaktivieren
+        InputSystem.Player.Disable();
+    }
+    void Start()
+    {
+        SellUI.SetActive(false);
+        CloseSellUI.onClick.AddListener(() =>
+        {
+            for (int i = 0; i < ViligerParent.transform.childCount; i++)
+            {
+                NavMeshAgent villager = ViligerParent.transform.GetChild(i).GetComponent<NavMeshAgent>();
+                villager.enabled = true;
+            }
+            SellUI.SetActive(false);
+        });
+    }
+
     void Update()
     {
+
+
+
         for (int i = 0; i < ViligerParent.transform.childCount; i++)
         {
             NavMeshAgent villager = ViligerParent.transform.GetChild(i).GetComponent<NavMeshAgent>();
 
+            if (InputSystem.Player.Interact.WasPressedThisFrame() && ViligerParent.transform.GetChild(i).GetComponent<Collider2D>().IsTouching(Player))
+            {
+                SellUI.SetActive(true);
+                villager.enabled = false;
+            }
+
+
+            if (villager == null) continue;
+
             if (villager.enabled && villager.isOnNavMesh)
             {
-                if (!villager.pathPending && villager.remainingDistance <= villager.stoppingDistance)
+                Vector3 velocity = villager.velocity;
+
+                Animator anim = ViligerParent.transform.GetChild(i).GetComponent<Animator>(); ;
+
+                if (velocity.magnitude < 0.1f)
                 {
-                    Vector3 newDestination = WalkToPoints[Random.Range(0, WalkToPoints.Count)].position;
-                    villager.SetDestination(newDestination);
+                    // Villager steht still → Idle Animation
+                    if (anim != null) anim.Play("Idle");
                 }
+                else
+                {
+                    // Bewegungsrichtung prüfen
+                    if (velocity.z > 0.1f) // läuft nach oben
+                    {
+                        if (anim != null) anim.Play("MoveUp");
+                    }
+                    else if (Mathf.Abs(velocity.x) > 0.1f) // seitlich
+                    {
+                        if (anim != null) anim.Play("MoveX");
+                    }
+                    else
+                    {
+                        if (anim != null) anim.Play("Idle"); // steht still
+                    }
+
+                }
+                bool arrived = !villager.pathPending && villager.remainingDistance <= villager.stoppingDistance;
+                if (arrived && !IsWaiting(villager))
+                {
+                    StartCoroutine(WaitForPath(villager));
+                }
+
             }
+
         }
+
     }
 
+    private void SetNewDestination(NavMeshAgent agent)
+    {
+        Vector3 newDestination = WalkToPoints[Random.Range(0, WalkToPoints.Count)].position;
+        agent.SetDestination(newDestination);
+    }
+
+    private bool IsWaiting(NavMeshAgent agent)
+    {
+        return isWaiting.ContainsKey(agent) && isWaiting[agent];
+    }
+
+    IEnumerator WaitForPath(NavMeshAgent navMeshAgent)
+    {
+        isWaiting[navMeshAgent] = true;
+
+        navMeshAgent.ResetPath();
+
+        yield return new WaitForSeconds(Random.Range(3, 10));
+
+        SetNewDestination(navMeshAgent);
+        isWaiting[navMeshAgent] = false;
+    }
 }
